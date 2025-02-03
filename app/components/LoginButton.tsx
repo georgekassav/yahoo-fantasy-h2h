fcf9c52e6fcd8317ea40c958604f3fb04140ed3f
Georgios Kassavetis <108403392+georgekassav@users.noreply.github.com>
2025-02-03 22:36:23 +0200

    feat: shadcn (#1)

--- app/components/LoginButton.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export const LoginButton: React.FC = () => {
-  const handleLogin = () => {
-    window.location.href = "/api/auth/login";
-  };
-
-  return <button onClick={handleLogin}>Log in with Yahoo</button>;
-};
